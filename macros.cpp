85d08c344db9e5b2d265baf1befdab157f54f566
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-04 08:22:11 +0200

    + macros.cpp

---
+++ macros.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+//  Compile with g++ -std=c++11 or g++ -std=c++14 to
+//  have different output.
+//  See also https://stackoverflow.com/a/7132549/180275
+    std::cout << "__cplusplus: " << __cplusplus << std::endl;
+}
