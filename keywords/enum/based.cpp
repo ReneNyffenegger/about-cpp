4d4335ef8582b08401ff8ee840b9a285a64b161d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-08 17:24:55 +0200

    + based.cpp

---
+++ keywords/enum/based.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+enum ENUM_char  : char  { a, b, c }; 
+enum ENUM_short : short { d, e, f };
+enum ENUM_int   : int   { g, h, i };
+enum ENUM_long  : long  { j, k, l };
+
+int main() {
+
+  ENUM_char  ec = a;
+  ENUM_short es = d;
+  ENUM_int   ei = i;
+  ENUM_long  el = j;
+
+  std::cout << sizeof(ec) << std::endl; // 1
+  std::cout << sizeof(es) << std::endl; // 2
+  std::cout << sizeof(ei) << std::endl; // 4
+  std::cout << sizeof(el) << std::endl; // 8
+
+}
